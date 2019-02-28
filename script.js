051a77a0a8e647714486c1718ba60608de1f4fc0
itscottpool <47753013+itscottpool@users.noreply.github.com>
2019-02-27 21:08:31 -0500

    Modified code for brackets
    updated default background colors and modified it for no errors in Brackets.

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
+/*eslint-env browser*/
+/*jslint node: true*/
+"use strict";
+
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 
 function setGradient() {
-	body.style.background = 
-	"linear-gradient(to right, " 
-	+ color1.value 
-	+ ", " 
-	+ color2.value 
-	+ ")";
-
+	body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
 	css.textContent = body.style.background + ";";
 }
 
